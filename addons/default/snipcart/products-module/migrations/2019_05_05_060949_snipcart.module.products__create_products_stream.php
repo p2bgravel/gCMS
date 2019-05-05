a94ce16a78f2e1e455223203bcb2fcfc0b4b172d
LeTranAnhVu <vule5726@gmail.com>
2019-05-05 15:04:13 +0700

    chức năng shop - dùng Snipcart - còn lỗi hiện thị text trong phần nhập sản phẩm ở trang admin/products

---
+++ addons/default/snipcart/products-module/migrations/2019_05_05_060949_snipcart.module.products__create_products_stream.php
@@ -0,0 +1,44 @@
+<?php
+
+use Anomaly\Streams\Platform\Database\Migration\Migration;
+
+class SnipcartModuleProductsCreateProductsStream extends Migration
+{
+
+    /**
+     * The stream definition.
+     *
+     * @var array
+     */
+    protected $stream = [
+        'slug' => 'products',
+        'title_column' => 'name',
+        'translatable' => true,
+        'versionable' => false,
+        'trashable' => false,
+        'searchable' => false,
+        'sortable' => false,
+    ];
+
+    /**
+     * The stream assignments.
+     *
+     * @var array
+     */
+    protected $assignments = [
+        'name' => [
+            'required' => true,
+        ],
+        'sku' => [
+            'unique' => true,
+            'required' => true,
+        ],
+        'price' => [
+            'required' => true
+        ],
+        'description',
+        'image',
+        'tags',
+    ];
+
+}
---
+++ addons/default/snipcart/products-module/src/Product/ProductPresenter.php
@@ -0,0 +1,61 @@
+<?php namespace Snipcart\ProductsModule\Product;
+
+use Anomaly\Streams\Platform\Entry\EntryPresenter;
+use Anomaly\FilesModule\File\Command\GetType;
+use Collective\Html\HtmlBuilder;
+
+class ProductPresenter extends EntryPresenter
+{
+    /**
+     * The HTML builder.
+     *
+     * @var HtmlBuilder
+     */
+    protected $html;
+
+    /**
+     * Create a new ProductPresenter instance.
+     *
+     * @param HtmlBuilder $html
+     * @param             $object
+     */
+    public function __construct(HtmlBuilder $html, $object)
+    {
+        $this->html = $html;
+
+        parent::__construct($object);
+    }
+
+    /**
+     * Return the full name.
+     *
+     * @return string
+     */
+    public function buyButton($text, array $attributes = [])
+    {
+        $class     = trim(array_get($attributes, 'class', '') . ' snipcart-add-item');
+        $imageType = $this->dispatch(new GetType($this->object->image)) ?: 'document';
+
+        $imageUrl = $this->object->image
+            ->make("anomaly.module.files::img/types/{$imageType}.png")
+            ->cropped()->widen(100)->path();
+
+        return $this->html->link(
+            '#',
+            $text,
+            array_merge($attributes, [
+                'class'                 => $class,
+                'data-item-id'          => $this->object->sku,
+                'data-item-name'        => $this->object->name,
+                'data-item-description' => $this->object->description,
+                'data-item-price'       => $this->object->price,
+                'data-item-url'         => '/products',
+                'data-item-image'       => $imageUrl,
+                'data-item-metadata'    => json_encode(array_combine(
+                    $this->object->tags,
+                    array_map(function ($tag) { return true; }, $this->object->tags)
+                )),
+            ])
+        );
+    }
+}
